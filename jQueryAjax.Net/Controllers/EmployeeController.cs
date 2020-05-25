7979256436f19856bcebff5a0ece40adaebb61d5
Guilherme da Silva <guilherme.dasilvanascimento@gmail.com>
2020-05-25 16:33:29 -0300

    Primeiro Commit

---
+++ jQueryAjax.Net/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using jQueryAjax.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryAjax.Net.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using(DBModel db = new DBModel())
+            {
+                return db.Employee.ToList<Employee>();
+            }
+        }
+
+        public ActionResult AddOrEdit(int id=0)
+        {
+            Employee emp = new Employee();
+            return View(emp);
+        }
+
+        [HttpPost]
+
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+    }
+}
